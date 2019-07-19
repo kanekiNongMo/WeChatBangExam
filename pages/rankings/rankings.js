5785b070d4335ad3c66d51679935faa60174016a
kaneki <1165201419@qq.com>
2019-07-19 17:12:31 +0800

    排行榜

--- pages/rankings/rankings.js
+++ pages/rankings/rankings.js
@@ -5,14 +5,75 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    rangking: [],
+    javaRangking: [],
+    testRangking: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var _this = this;
+    wx.request({
+      url: 'http://127.0.0.1:8888/score/ranking',
+      // data: {
+      //   start: 0,
+      //   limit: 3
+      // },
+      success: function (data) {
+        console.log(data.data);
+        _this.setData({
+          rangking: data.data
+        })
+      },
+      fail: function (data) {
+        wx.showToast({
+          title: '网络请求失败',
+          icon: 'none'
+        })
+      }
+    }),
+      wx.request({
+        url: 'http://127.0.0.1:8888/score/ranking',
+        data: {
+          // start: 0,
+          // limit: 3,
+          majorType: 1
+        },
+        success: function (data) {
+          console.log(data.data);
+          _this.setData({
+            javaRangking: data.data
+          })
+        },
+        fail: function (data) {
+          wx.showToast({
+            title: '网络请求失败',
+            icon: 'none'
+          })
+        }
+      }),
+      wx.request({
+        url: 'http://127.0.0.1:8888/score/ranking',
+        data: {
+          // start: 0,
+          // limit: 3,
+          majorType: 2
+        },
+        success: function (data) {
+          console.log(data.data);
+          _this.setData({
+            testRangking: data.data
+          })
+        },
+        fail: function (data) {
+          wx.showToast({
+            title: '网络请求失败',
+            icon: 'none'
+          })
+        }
+      })
   },
 
   /**
@@ -62,5 +123,24 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  onOpenRankingDetail: function (event) {
+    var ranking = event.currentTarget.dataset.ranking;
+    console.log(ranking);
+    wx.setStorage({
+      key: "ranking",
+      data: ranking
+    })
+    wx.setStorage({
+      key: "javaRangking",
+      data: ranking
+    })
+    wx.setStorage({
+      key: "testRangking",
+      data: ranking
+    })
+    wx.navigateTo({
+      url: "./rankings-detail"
+    })
   }
 })
